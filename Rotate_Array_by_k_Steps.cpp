285c197f35b08a456426afef683124906b0b4374
Shweta Singh <shwetasingh99647@gmail.com>
2026-03-11 00:38:22 +0530

    Add files via upload

---
+++ Rotate_Array_by_k_Steps.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+    int n,k;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    cin>>k;
+
+    k = k % n;
+
+    reverse(arr, arr+n);
+    reverse(arr, arr+k);
+    reverse(arr+k, arr+n);
+
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+
+}
